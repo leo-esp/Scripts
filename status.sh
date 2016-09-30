a24f29faa18b9363a69551cd9058080bb954c448
leo-esp <leo_esp_5@hotmail.com>
2016-09-30 12:45:19 -0300

    retirando status

--- status.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-array=( $(find . -mindepth 1 -maxdepth 1 -not -path '*/\.*' -type d) )
-
-# loop over it
-for i in ${array[@]}
-do
-    echo "project: "$i
-    eval "git status ${array[$i]}"
-done
